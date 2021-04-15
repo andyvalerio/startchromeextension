cb9b1f3c526dca04b52c350dd9d4377818ab6375
andyvandermeyde <giuseppe@valerio.nu>
2021-04-15 09:32:34 +0200

    Initialize spring-boot project

---
+++ src/main/java/com/gv/sce/StartchromeextensionApplication.java
@@ -0,0 +1,13 @@
+package com.gv.sce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartchromeextensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartchromeextensionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gv/sce/StartchromeextensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gv.sce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartchromeextensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
